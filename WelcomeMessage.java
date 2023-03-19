83a8ac8944839e8b43826fd8a19372ec51ad79f0
milindkumar64 <milindgupta@gmail.com>
2023-03-19 23:42:18 +0530

    Add: Welcome Message

---
+++ WelcomeMessage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snackladderprogram;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Snake Ladder game, Let's Play ");
+	}
+
+}
