36f5f47bb4f1e27a10cce09d7ed3b1e6230c9af7
milindkumar64 <milindgupta@gmail.com>
2023-03-20 00:56:42 +0530

    Add: Reporting the number of rolls of Dice to win and position of every die

--- src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
+++ src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
@@ -12,6 +12,7 @@ public class SnackLadderGame {
 	    
 		    int position = 0;
 		    int diceNumber=0;
+		    int countRolls=0;
 	        Random ranNum = new Random(); 
 	        
 	    while(position<100)
@@ -24,20 +25,25 @@ public class SnackLadderGame {
 	        {   
 	        	diceNumber = ranNum.nextInt(6)+1;
 	        	position += diceNumber;
+	        	countRolls++;
 	        }else
 	        {
 	        	diceNumber = ranNum.nextInt(6)+1;
 	        	position -=diceNumber;
+	        	countRolls++;
+	        	System.out.println("Dice got died :"+position);
 	        }
 	        if(position<0)
 	        	position = 0;
-	        if(position==100)
-	        	break;
+			
+			 if(position==100) 
+				 break;
+			 
 	        if(position>100)
 	        	position=position-diceNumber;
 	        System.out.println("Player current position is : "+position);
 	    }   
-	        
+	        System.out.println("Number of Rolls :"+countRolls);
 	        
 	        System.out.println("Player current position is : "+position);
 
