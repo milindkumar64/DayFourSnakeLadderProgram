4fbe794f5f7c5f69434b0af31bd14738f6894e72
milindkumar64 <milindgupta@gmail.com>
2023-03-20 00:20:21 +0530

    Add: Check the play option like No play, Ladder or Snake

--- src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
+++ src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
@@ -6,13 +6,35 @@ public class SnackLadderGame {
 
 	public static void main(String[] args) {
 		
-        int position = 0;
-        Random ranNum = new Random(); 
-        
-        int diceNumber = ranNum.nextInt(6)+1;
-        
-		
-        System.out.println(diceNumber);
+		int NO_PLAY=0;
+	    int LADDER=1;
+	    int SNAKE=2;
+	    
+		    int position = 0;
+		    int diceNumber=0;
+	        Random ranNum = new Random(); 
+	        
+	       
+	        int playOption = ranNum.nextInt(3);
+	        
+	        if(playOption == NO_PLAY)
+	        	position = position+0;
+	        else if(playOption == LADDER)
+	        {   
+	        	diceNumber = ranNum.nextInt(6)+1;
+	        	position += diceNumber;
+	        }else
+	        {
+	        	diceNumber = ranNum.nextInt(6)+1;
+	        	position -=diceNumber;
+	        }
+	        if(position<0)
+	        	position = 0;
+	        
+	        
+	        
+	        System.out.println("Player current position is : "+position);
+
 	}
 
 }
