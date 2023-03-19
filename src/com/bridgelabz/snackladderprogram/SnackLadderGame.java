de02c5c3e94b8f0a5e0d4f9d82c6d3c4f07b16b6
milindkumar64 <milindgupta@gmail.com>
2023-03-20 00:14:25 +0530

    Add: Rolls the dice and get Number

--- src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
+++ src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
@@ -1,12 +1,18 @@
 package com.bridgelabz.snackladderprogram;
 
+import java.util.Random;
+
 public class SnackLadderGame {
 
 	public static void main(String[] args) {
 		
-		int position = 0;
+        int position = 0;
+        Random ranNum = new Random(); 
+        
+        int diceNumber = ranNum.nextInt(6)+1;
+        
 		
-		System.out.println("Player current position is : "+position);
+        System.out.println(diceNumber);
 	}
 
 }
