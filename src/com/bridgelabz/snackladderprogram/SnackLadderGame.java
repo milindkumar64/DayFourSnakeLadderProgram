841db03f5d1d664d496107eb56626c9a695cf143
milindkumar64 <milindgupta@gmail.com>
2023-03-20 00:01:12 +0530

    Add: Start With Zero Position

---
+++ src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snackladderprogram;
+
+public class SnackLadderGame {
+
+	public static void main(String[] args) {
+		
+		int position = 0;
+		
+		System.out.println("Player current position is : "+position);
+	}
+
+}
