4a304bd0dbc782f3e0fa73db8d068c90de5a7052
milindkumar64 <milindgupta@gmail.com>
2023-03-22 01:12:09 +0530

    Add: Play With Two Players and see Who won the the Game

--- src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
+++ src/com/bridgelabz/snackladderprogram/SnackLadderGame.java
@@ -10,42 +10,80 @@ public class SnackLadderGame {
 	    int LADDER=1;
 	    int SNAKE=2;
 	    
-		    int position = 0;
+		    int position1 = 0;
+		    int position2 = 0;
+		    int playOption=0;
 		    int diceNumber=0;
-		    int countRolls=0;
+		    int countRolls1=0;
+		    int countRolls2=0;
 	        Random ranNum = new Random(); 
 	        
-	    while(position<100)
-	    {	  
-	        int playOption = ranNum.nextInt(3);
+	    while(position1<100 && position2<100)
+	    {	
+	    	// player 1
+	         playOption = ranNum.nextInt(3);
 	        
 	        if(playOption == NO_PLAY)
-	        	position = position+0;
+	        	position1 = position1+0;
 	        else if(playOption == LADDER)
 	        {   
 	        	diceNumber = ranNum.nextInt(6)+1;
-	        	position += diceNumber;
-	        	countRolls++;
+	        	position1 += diceNumber;
+	        	countRolls1++;
 	        }else
 	        {
 	        	diceNumber = ranNum.nextInt(6)+1;
-	        	position -=diceNumber;
-	        	countRolls++;
-	        	System.out.println("Dice got died :"+position);
+	        	position1 -=diceNumber;
+	        	countRolls1++;
+	        	//System.out.println("Dice got died :"+position1);
 	        }
-	        if(position<0)
-	        	position = 0;
+	        if(position1<0)
+	        	position1 = 0;
 			
-			 if(position==100) 
+			 if(position1==100) 
 				 break;
 			 
-	        if(position>100)
-	        	position=position-diceNumber;
-	        System.out.println("Player current position is : "+position);
-	    }   
-	        System.out.println("Number of Rolls :"+countRolls);
+	        if(position1>100)
+	        	position1=position1-diceNumber;
+	       // System.out.println("Player1 current position is : "+position1);
 	        
-	        System.out.println("Player current position is : "+position);
+	       // player 2
+	        
+              playOption = ranNum.nextInt(3);
+	        
+	        if(playOption == NO_PLAY)
+	        	position2 = position2+0;
+	        else if(playOption == LADDER)
+	        {   
+	        	diceNumber = ranNum.nextInt(6)+1;
+	        	position2 += diceNumber;
+	        	countRolls2++;
+	        }else
+	        {
+	        	diceNumber = ranNum.nextInt(6)+1;
+	        	position2 -=diceNumber;
+	        	countRolls2++;
+	        	//System.out.println("Dice got died :"+position1);
+	        }
+	        if(position2<0)
+	        	position2 = 0;
+			
+			 if(position2==100) 
+				 break;
+			 
+	        if(position2>100)
+	        	position2=position2-diceNumber;
+	    }   
+	        //System.out.println("player 1 Number of Rolls :"+countRolls1);
+	        //System.out.println("player 2 Number of Rolls :"+countRolls2);
+	        
+	        System.out.println("Player1 Final position is : "+position1);
+	        System.out.println("Player2 Final position is : "+position2);
+	        
+	        if(position1==100)
+	        	System.out.println("woo-hoo! Player 1 Won the Game : ");
+	        else
+	        	System.out.println("woo-hoo! Player 2 Won the Game : ");
 
 	}
 
